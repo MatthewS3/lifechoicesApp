cecebb53605bbcee4216253837c2e479a309747a
MatthewS3 <mtthwswain@gmail.com>
2024-02-15 09:25:04 +0200

    configcontent

---
+++ config/index.js
@@ -0,0 +1,14 @@
+import { createPool } from "mysql";
+import { config } from "dotenv";
+config();
+let connection = createPool({
+    host: process.env.HOST,
+    database: process.env.DBName,
+    user: process.env.UserName,
+    password: process.env.UserPass,
+    multipleStatements: true,
+    connectionLimit:30
+})
+export {
+    connection
+}
