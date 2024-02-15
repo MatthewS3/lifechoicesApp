a02643350ed322a003cb7069bbb098d6e0cd7bec
MatthewS3 <mtthwswain@gmail.com>
2024-02-15 11:36:05 +0200

    middleware

---
+++ middleware/UserAuthentication.js
@@ -0,0 +1,39 @@
+import { config } from "dotenv";
+config()
+import { sign, verify } from "jsonwebtoken";
+
+function createToken(user) {
+    return sign ({
+        emailAdd: user.emailAdd,
+        userPwd:  user.userPwd,    
+    },
+    process.env.SECRET_KEY,
+    {
+        expiresIn: '1h' // expires in 1 hour
+    }
+    )
+}
+
+function verifyAToken(req, res, next) {
+    // Retrive a token from the browser
+    const token = req.headers['Autherization']
+    if(token) {
+        if(verify(token, process.env.SECRET_KEY)) {
+            next()
+        }else {
+            res?.json({
+                status: res.statusCode,
+                msg: "Please provide the current credentials"
+            })
+        }
+    }else {
+        res?.json({
+            status: res.statusCode,
+            msg: "Please login"
+        })
+    }
+}
+export {
+    createToken,
+    verifyAToken
+}
