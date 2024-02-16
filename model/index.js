4e66245febbe9b4af13d1e227a6727ef16cb2a35
MatthewS3 <mtthwswain@gmail.com>
2024-02-16 14:17:44 +0200

    added index

--- model/index.js
+++ model/index.js
@@ -1,5 +1,5 @@
-import { Users } from "./Users";
-import { Products } from "./Products";
+import { Users } from "./Users.js";
+import { Products } from "./Products.js";
 // CREATE OBJECTS
 let users = new Users()
 let products = new Products()
